f841d56412d9952661356a71be1fe55fcd202ec0
Chetan Surwade <chetansurwade@outlook.com>
2022-01-24 14:33:19 +0530

    Create main.py

---
+++ projects/dummy_project/main.py
@@ -0,0 +1,10 @@
+def print_func1():
+    print('Hello from print_func1, updated in dev branch')
+
+
+def print_func2():
+    print('Hello from print_func2')
+
+
+def print_func3():
+    print('Hello from print_func3')
