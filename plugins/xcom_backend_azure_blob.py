23a3f8726acac757c2559e5427cc8a066a5163c4
Chetan Surwade <chetansurwade@outlook.com>
2022-02-25 13:11:44 +0530

    Update xcom_backend_azure_blob.py
    json deserialization causes issues sometime.

--- plugins/xcom_backend_azure_blob.py
+++ plugins/xcom_backend_azure_blob.py
@@ -1,7 +1,6 @@
 from typing import Any
 from airflow.models.xcom import BaseXCom
 from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
-import json
 import pandas as pd
 import uuid
 
@@ -15,22 +14,7 @@ class XcomAzureBlobBackend(BaseXCom):
             hook        = WasbHook()
             key         = "data_csv_" + str(uuid.uuid4())
             filename    = f"{key}.csv"
-
             value.to_csv(filename,index=False)
-            hook.load_file(
-                file_path=filename,
-                blob_name=key,
-                container_name=XcomAzureBlobBackend.CONTAINER_NAME
-            )
-            value = XcomAzureBlobBackend.PREFIX + key
-        if not isinstance(value, (str, dict, list, pd.DataFrame)):
-            hook        = WasbHook()
-            key         = "data_json_" + str(uuid.uuid4())
-            filename    = f"{key}.json"
-
-            with open(filename, 'w') as f:
-                json.dump(json.loads(str(value)), f)
-
             hook.load_file(
                 file_path=filename,
                 blob_name=key,
@@ -45,21 +29,16 @@ class XcomAzureBlobBackend(BaseXCom):
         if isinstance(result, str) and result.startswith(XcomAzureBlobBackend.PREFIX):
             hook    = WasbHook()
             key     = result.replace(XcomAzureBlobBackend.PREFIX, "")
-            file_format = key.split('_')[1]
-            filename = f"{key}.{file_format}"
+            filename = f"{key}.csv"
             hook.get_file(
                 file_path=filename,
                 container_name=XcomAzureBlobBackend.CONTAINER_NAME,
                 blob_name=key
             )
-            if file_format == 'csv':
-                result = pd.read_csv(filename)
-            else:
-                result = json.load(filename)
             hook.delete_file(
                 container_name=XcomAzureBlobBackend.CONTAINER_NAME,
                 blob_name=key,
                 ignore_if_missing=True
             )
-            
+            result = pd.read_csv(filename)
         return result
